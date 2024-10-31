603cc1c27aa704698bc783f893a414cee56c2316
Ashutosh Shukla <ashutoshshukla8970@gmail.com>
2024-11-01 00:51:04 +0530

    Added routes in index.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import ConnectDB from './db/index.js';
 import dotenv from 'dotenv';
+import userRoute from './routes/userRoute.js'
+import transactionRoute from './routes/tranactionRoute.js'
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
 ConnectDB();
+
+app.use('/api',userRoute);
+app.use('/api/transaction',transactionRoute);
 
 app.listen(process.env.PORT || 300);
