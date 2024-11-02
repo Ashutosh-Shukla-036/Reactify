808b6faca0f61ccdde1cb09b2f193f23e036aa28
Ashutosh Shukla <ashutoshshukla8970@gmail.com>
2024-11-02 15:39:43 +0530

    Added get user route

--- BackEnd/routes/userRoute.js
+++ BackEnd/routes/userRoute.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { getAllUsers, Login, SignUp } from '../Controllers/userController.js';
+import { getUser, Login, SignUp } from '../Controllers/userController.js';
 import { authMiddleware } from '../Middleware/authMiddleware.js';
 const Route = express.Router();
 
 Route.post('/signup',SignUp);
 Route.post('/login',Login);
-Route.get('/getusers',authMiddleware,getAllUsers);
+Route.get('/getusers',authMiddleware,getUser);
 
 export default Route;
