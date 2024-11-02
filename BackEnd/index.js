397f4932fa178840f9e6954d686094cbaa19de08
Ashutosh Shukla <ashutoshshukla8970@gmail.com>
2024-11-02 15:41:56 +0530

    added friend route in index.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -3,14 +3,18 @@ import ConnectDB from './db/index.js';
 import dotenv from 'dotenv';
 import userRoute from './routes/userRoute.js'
 import transactionRoute from './routes/tranactionRoute.js'
+import friendRoute from './routes/friendRoute.js'
+import cors from 'cors'
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 ConnectDB();
 
 app.use('/api',userRoute);
 app.use('/api/transaction',transactionRoute);
+app.use('/api/friend',friendRoute);
 
 app.listen(process.env.PORT || 300);
