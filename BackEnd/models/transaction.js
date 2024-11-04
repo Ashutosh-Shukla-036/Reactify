017145e2a2f2e052f8b358fe962ca343029cebda
Ashutosh Shukla <ashutoshshukla8970@gmail.com>
2024-11-04 20:47:10 +0530

    correct date and time by using moment-timezone

--- BackEnd/models/transaction.js
+++ BackEnd/models/transaction.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
+import moment from 'moment-timezone'
 import z from 'zod';
+
+const getCurrentDateIndia = () => moment.tz(Date.now(), "Asia/Kolkata").toDate();
 
 export const validateAmount = z.object({
     amount: z.number().min(1, { message: 'Amount must be at least 1' })
@@ -9,7 +12,7 @@ const transactionSchema = mongoose.Schema({
     sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     receiver: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     amount : { type: Number, required: true },
-    timestamp: { type: Date, default: Date.now }
+    timestamp: { type: Date, default: getCurrentDateIndia, required: true, }
 });
 
 const Transaction = mongoose.model('Transaction', transactionSchema);
