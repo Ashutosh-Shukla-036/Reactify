9a5de4bfcad35badb4083d72bd82637471c54b20
Ashutosh Shukla <ashutoshshukla8970@gmail.com>
2024-11-04 10:18:52 +0530

    Updated auth middleware, removed all logs

--- BackEnd/Middleware/authMiddleware.js
+++ BackEnd/Middleware/authMiddleware.js
@@ -11,14 +11,11 @@ export const authMiddleware = (req, res, next) => {
 
     const token = authHeader.split(' ')[1];
 
-    console.log("Token received:", token); 
-
     try {
         const decoded = jwt.verify(token, process.env.JWT_KEY); 
         req.user = decoded;
         next();
     } catch (error) {
-        console.error("Token verification failed:", error.message); 
         return res.status(401).json({ message: 'Invalid token' });
     }
 };
