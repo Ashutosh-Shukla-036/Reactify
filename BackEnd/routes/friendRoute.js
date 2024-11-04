d0d37ee8378abc10cf8a6ff992595804a822425b
Ashutosh Shukla <ashutoshshukla8970@gmail.com>
2024-11-05 01:34:12 +0530

    updated friends route

--- BackEnd/routes/friendRoute.js
+++ BackEnd/routes/friendRoute.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { authMiddleware } from '../Middleware/authMiddleware.js';
-import { addFriend, getFriends } from '../Controllers/friendController.js';
+import { addFriend, getFriends, searchFriend } from '../Controllers/friendController.js';
 
 const Route = express.Router();
 
-Route.get('/getfriend',authMiddleware,getFriends);
+Route.get('/getfriend/:userId',authMiddleware,getFriends);
 Route.post('/addfriend',authMiddleware,addFriend);
+Route.get('/searchfriend/:username',authMiddleware,searchFriend);
 
 export default Route;
