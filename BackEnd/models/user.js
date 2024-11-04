00d7f712b9553da3af1e610a6df32d56af5e711f
Ashutosh Shukla <ashutoshshukla8970@gmail.com>
2024-11-04 20:48:11 +0530

    Made default balance as 500

--- BackEnd/models/user.js
+++ BackEnd/models/user.js
@@ -2,14 +2,14 @@ import mongoose from "mongoose";
 import z from 'zod';
 
 export const validateUser = z.object({
-    username: z.string().min(1, { message: "username is required" }),
+    username: z.string().min(1).max(16,{ message: "username is required. username can have maximum 16 characters." }),
     password: z.string().min(6, { message: "Password should have minimum 6 characters" })
 });
 
 const UserSchema = mongoose.Schema({
     username : { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    balance: { type: Number, required: true, default: 0 }
+    balance: { type: Number, required: true, default: 500 }
 });
 
 const User = mongoose.model('User', UserSchema);
