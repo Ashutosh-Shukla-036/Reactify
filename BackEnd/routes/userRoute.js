010e89b139be6b658175e22e26eef2dd64538e7b
Ashutosh Shukla <ashutoshshukla8970@gmail.com>
2024-11-16 23:51:19 +0530

    Added route to update user in real time

--- BackEnd/routes/userRoute.js
+++ BackEnd/routes/userRoute.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getUser, Login, SignUp } from '../Controllers/userController.js';
+import { getUser, Login, SignUp, UserUpdate } from '../Controllers/userController.js';
 import { authMiddleware } from '../Middleware/authMiddleware.js';
 const Route = express.Router();
 
 Route.post('/signup',SignUp);
 Route.post('/login',Login);
 Route.get('/getusers',authMiddleware,getUser);
+Route.get('/userupdate/:userId',authMiddleware,UserUpdate);
 
 export default Route;
