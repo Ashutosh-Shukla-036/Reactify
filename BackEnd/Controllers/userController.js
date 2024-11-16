bf1eb8be228ca4b9963c2fe936c7ff2ea155c591
Ashutosh Shukla <ashutoshshukla8970@gmail.com>
2024-11-16 23:50:30 +0530

    Added update User route to update user in real time

--- BackEnd/Controllers/userController.js
+++ BackEnd/Controllers/userController.js
@@ -72,3 +72,19 @@ export const getUser = async (req, res) => {
         return res.status(500).json({ message: `Internal server error ${error.message}` });
     }
 };
+
+export const UserUpdate = async (req, res) => {
+    const { userId } = req.params; 
+
+    try {
+        const user = await User.findById(userId).select('_id username balance');
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        return res.status(200).json(user);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: `Internal server error: ${error.message}` });
+    }
+};
