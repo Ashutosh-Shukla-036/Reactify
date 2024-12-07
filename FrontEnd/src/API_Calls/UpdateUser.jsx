2cf48e37e902124cdec9b504b1a787d952c6a8f0
Ashutosh Shukla <ashutoshshukla8970@gmail.com>
2024-12-07 11:12:48 +0530

    Updated update user API call

--- FrontEnd/src/API_Calls/UpdateUser.jsx
+++ FrontEnd/src/API_Calls/UpdateUser.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export const UpdateUser = async (userId) => {
     try {
+        console.log(userId)
         const response = await axios.get(`https://reactify-i1sa.onrender.com/api/userupdate/${userId}`, {
             headers: {
                 'Content-Type': 'application/json',
